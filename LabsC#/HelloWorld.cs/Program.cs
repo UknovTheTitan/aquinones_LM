99f3a4d7eab5c61dd0219a6004f092ceb0c6be78
Anthony Quiñones <anthonytitan90@gmail.com>
2024-03-04 09:44:45 -0400

    "java app"

--- LabsC#/HelloWorld.cs/Program.cs
+++ LabsC#/HelloWorld.cs/Program.cs
@@ -1,12 +1,2 @@
-﻿using System;
-
-namespace HelloWorld
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World, mundo especial!");
-        }
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
